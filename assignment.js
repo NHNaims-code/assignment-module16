d1b808e3f6b518eb9b663a1c498d78bbd3330b44
NHNaims-code <mdnimurhasan99@gmail.com>
2020-07-16 23:26:27 -0700

    first commit

---
+++ assignment.js
@@ -0,0 +1,69 @@
+function feetToMile(feet) {
+    if (feet < 0) {
+        return 'distance cannot be negative.';
+    } else {
+        var oneFeetToMile = 1 / 5280;
+        var resultMile = feet * oneFeetToMile;
+        return resultMile;
+    }
+
+}
+
+
+function woodCalculator(chair, table, bed) {
+    if (chair < 0 || table < 0 || bed < 0) {
+        return 'chair, table or bed number cannot be negative.';
+    } else {
+        var totalChairWood = chair * 1;
+        var totalTableWood = table * 3;
+        var totalBedWood = bed * 5
+        var totalWood = totalChairWood + totalTableWood + totalBedWood;
+        return totalWood;
+    }
+}
+
+
+function brickCalculator(floor) {
+    if (floor < 0) {
+        return 'Your input cannot be negative.';
+    } else {
+        var totalFeet = 0;
+        if (floor < 11) {
+            totalFeet = floor * 15;
+        } else if (floor < 21 && 10 < floor) {
+            totalFeet = 10 * 15;
+            totalFeet = totalFeet + (floor - 10) * 12;
+        } else if (floor > 20) {
+            totalFeet = 10 * 15;
+            totalFeet = totalFeet + (10 * 12);
+            totalFeet = totalFeet + (floor - 20) * 10;
+        }
+        var totalBrick = totalFeet * 1000;
+        return totalBrick;
+    }
+}
+
+
+function tinyFriends(nameList) {
+    var uniqueNameList = [];
+    for (var i = 0; i < nameList.length; i++) {
+        if (nameList[i] != ' ') {
+            uniqueNameList.push(nameList[i])
+        }
+    }
+    var nameCharacterList = [];
+    for (var i = 0; i < uniqueNameList.length; i++) {
+        var count = 0;
+        var fName = uniqueNameList[i];
+        for (var j = 0; j < fName.length; j++) {
+            if (fName[j] != " ") {
+                count++;
+            }
+        }
+        nameCharacterList.push(count);
+    }
+    var minCharacter = Math.min(...nameCharacterList);
+    var tinyNameIndex = nameCharacterList.indexOf(minCharacter);
+    var tinyName = uniqueNameList[tinyNameIndex];
+    return tinyName;
+}
